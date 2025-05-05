3acbf9ba1112229589e3af883664a6c96ac4c123
DESKTOP-H1PEDVE\Shunsuke_Hata <hatajapan.0710@gmail.com>
2025-05-05 16:55:38 +0900

    fix

--- envs/dev/souvenirConsultApp/main.tf
+++ envs/dev/souvenirConsultApp/main.tf
@@ -1,12 +1,12 @@
-#module "application_data_bucket" {
-#  # モジュールディレクトリへの相対パスを指定
-#  source = "../../../modules/s3-module"
-#
-#  bucket_name = "terraform-bucket-2025-5-5"
-#
-#  aws_region = "ap-northeast-1"
-#
-#}
+module "application_data_bucket" {
+  # モジュールディレクトリへの相対パスを指定
+  source = "../../../modules/s3-module"
+
+  bucket_name = "terraform-bucket-2025-5-5"
+
+  aws_region = "ap-northeast-1"
+
+}
 
 module "application_role" {
   # モジュールディレクトリへの相対パスを指定
