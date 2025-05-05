4ebe727d04574d37a9e3e75e36f5d2670e9156d0
DESKTOP-H1PEDVE\Shunsuke_Hata <hatajapan.0710@gmail.com>
2025-05-05 16:53:59 +0900

    fix

--- envs/dev/souvenirConsultApp/main.tf
+++ envs/dev/souvenirConsultApp/main.tf
@@ -1,33 +1,33 @@
-module "application_data_bucket" {
-  # モジュールディレクトリへの相対パスを指定
-  source = "../../../modules/s3-module"
-
-  bucket_name = "terraform-bucket-2025-5-5"
-
-  aws_region = "ap-northeast-1"
-
-}
-
-#module "application_role" {
+#module "application_data_bucket" {
 #  # モジュールディレクトリへの相対パスを指定
-#  source = "../../../modules/iam-role-module"
+#  source = "../../../modules/s3-module"
 #
-#  role_name = "souvenir-consult-app-role"
+#  bucket_name = "terraform-bucket-2025-5-5"
 #
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Effect = "Allow"
-#        Principal = {
-#          Service = "lambda.amazonaws.com"
-#        }
-#        Action = "sts:AssumeRole"
-#      }
-#    ]
-#  })
-#
-#  # モジュール側の変数 aws_region に明示的に東京リージョンを渡す
 #  aws_region = "ap-northeast-1"
 #
 #}
+
+module "application_role" {
+  # モジュールディレクトリへの相対パスを指定
+  source = "../../../modules/iam-role-module"
+
+  role_name = "souvenir-consult-app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  # モジュール側の変数 aws_region に明示的に東京リージョンを渡す
+  aws_region = "ap-northeast-1"
+
+}
