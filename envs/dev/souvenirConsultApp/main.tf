87a764e074d1b936e90aa583eacb359285697d12
DESKTOP-H1PEDVE\Shunsuke_Hata <hatajapan.0710@gmail.com>
2025-05-05 19:04:12 +0900

    fix

--- envs/dev/souvenirConsultApp/main.tf
+++ envs/dev/souvenirConsultApp/main.tf
@@ -8,3 +8,26 @@ module "application_data_bucket" {
 
 }
 
+module "application_role" {
+  # モジュールディレクトリへの相対パスを指定
+  source = "../../../modules/iam-role-module"
+
+  role_name = "souvenir-consult-app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  # モジュール側の変数 aws_region に明示的に東京リージョンを渡す
+  aws_region = "ap-northeast-1"
+
+}
